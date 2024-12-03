259279139249417f8824894684c8f2bf3f155293
vitorlosing1 <vitorluricii@hotmail.com>
2024-12-03 11:22:52 -0300

    header-start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import "./App.scss";
+import { LogoIcon } from "./assets/svg/LogoIcon";
 
 function App() {
   return (
-    <>
-      <p className="read-the-docs">Vitor Portfólio</p>
-    </>
+    <main className="main-container">
+      <header className="header-container">
+        <div className="logo">
+          <LogoIcon />
+          <div className="logo-text">
+            <span className="name">Vitor Lurici</span>
+            <span className="description">SOFTWARE ENGINEER</span>
+          </div>
+        </div>
+        <div className="menu">
+          <span>MENU</span>
+        </div>
+      </header>
+    </main>
   );
 }
 
