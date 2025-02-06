1947a252645b66e1877e29fd3e824129aff5a6b1
vitorlosing1 <vitorluricii@hotmail.com>
2025-02-05 23:00:44 -0300

    projects-update

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import { useRef } from "react";
 
 function App() {
   const projectsRef = useRef<HTMLDivElement | null>(null);
+  const aboutMeRef = useRef<HTMLDivElement | null>(null);
 
   const scrollToProjects = () => {
     if (projectsRef.current) {
       projectsRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  const scrollToAboutMe = () => {
+    if (aboutMeRef.current) {
+      aboutMeRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -31,7 +37,7 @@ function App() {
         <div className="introduction">
           <div className="content-left">
             <span>VITOR LURICI</span>
-            <h1>Front-end and Back-end Engineer</h1>
+            <h1>Software Engineer</h1>
             <h4>
               During my three years as a software engineering student, I honed
               strong critical and analytical skills, consistently delivering
@@ -40,7 +46,7 @@ function App() {
             <div className="main-links">
               <button onClick={scrollToProjects}>View Projects</button>
               <p>or</p>
-              <button onClick={scrollToProjects}>Read About Me</button>
+              <button onClick={scrollToAboutMe}>Read About Me</button>
             </div>
           </div>
           <div className="content-right"></div>
@@ -77,34 +83,44 @@ function App() {
                 <p>2024</p>
                 <h2>COSMOS</h2>
                 <p>
-                  A captivating space exploration site utilizing NASA APIs,
+                  A captivating space exploration Website utilizing NASA APIs,
                   Google Translate, and Spaceflight News API. Developed with
                   React + Vite, JavaScript, Firebase, and SCSS.
                 </p>
                 <p className="view-git">View Project on GitHub →</p>
               </div>
             </a>
-            <div className="item">
-              <p>2024</p>
-              <h2>COSMOS</h2>
-              <p>
-                A captivating space exploration site utilizing NASA APIs, Google
-                Translate, and Spaceflight News API. Developed with React, Vite,
-                JavaScript, Firebase, and SCSS.
-              </p>
-              <p className="view-git">View Project on GitHub →</p>
-            </div>
-            <div className="item">
-              <p>2024</p>
-              <h2>COSMOS</h2>
-              <p>
-                A captivating space exploration site utilizing NASA APIs, Google
-                Translate, and Spaceflight News API. Developed with React, Vite,
-                JavaScript, Firebase, and SCSS.
-              </p>
-              <p className="view-git">View Project on GitHub →</p>
-            </div>
+            <a
+              href="https://github.com/vitorlurici/finance-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="item finance">
+                <p>2023 - In Progress</p>
+                <h2>FINANCE</h2>
+                <p>
+                  A captivating space exploration site utilizing NASA APIs,
+                  Google Translate, and Spaceflight News API. Developed with
+                  React, Vite, JavaScript, Firebase, and SCSS.
+                </p>
+                <p className="view-git">View Project on GitHub →</p>
+              </div>
+            </a>
           </div>
+        </div>
+        <div ref={aboutMeRef} className="about-me">
+          <div className="title">
+            <span>A FEW WORDS</span>
+            <h1>About Me</h1>
+          </div>
+          <h3>
+            I'm Vitor Lurici, an aspiring Software Engineer with a solid
+            foundation in software development and a proven ability to
+            collaborate effectively in team environments. Currently pursuing a
+            degree in Software Engineering, I have gained hands-on experience
+            through personal and academic projects, demonstrating a passion for
+            innovation and problem-solving.
+          </h3>
         </div>
       </div>
     </main>
